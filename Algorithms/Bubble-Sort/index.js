79045b98b725351feecb2cf787bcfbd4e616bd03
Julian Alexander <julian.pineyro@gmail.com>
2018-07-10 01:09:20 +0100

    Updated

--- Algorithms/Bubble-Sort/index.js
+++ Algorithms/Bubble-Sort/index.js
@@ -7,6 +7,16 @@ Testing: `jest Bubble-Sort/test.js`
 */
 
 function bubbleSort(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let lesser = arr[j + 1];
+                arr[j+1] = arr[j];
+                arr[j] = lesser;
+            }
+        }
+    }
+    return arr;
 }
 
 module.exports = bubbleSort;
